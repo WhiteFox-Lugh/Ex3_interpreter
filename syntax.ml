a8461d0624c0726e28c2609e8654ccedab736408
WhiteFox <lugh@Lugh-whitefox.local>
2018-06-26 15:56:29 +0900

    required exercises all completed (re-push)

--- syntax.ml
+++ syntax.ml
@@ -1,7 +1,9 @@
 (* ML interpreter / type reconstruction *)
 type id = string
 
+
 type binOp = Plus | Mult | Lt | And | Or
+
 
 type exp =
   | Var of id (* Var "x" --> x *)
@@ -22,19 +24,23 @@ type exp =
     (* ML4 interpreter *)
   | LetRecExp of id * id * exp * exp
 
+
 type program = 
     Exp of exp
   | Decl of id * exp
   | RecDecl of id * id * exp
 
+
 (* Static type inference *)
 type tyvar = int
+
 
 type ty =
     TyInt
   | TyBool
   | TyVar of tyvar
   | TyFun of ty * ty
+
 
 (* pretty printing *)
 let fresh_tyvar =
@@ -44,16 +50,40 @@ let fresh_tyvar =
         counter := v + 1; v
     in body
 
-let rec pp_ty = function
-    TyInt -> print_string "int"
-  | TyBool -> print_string "bool"
-  | TyVar _ -> print_string (string_of_int (fresh_tyvar ()))
+
+let pp_ty ty =
+  (* 型変数が 1 -> 2 ではなく 'a -> 'b となるように処理 *)
+  let var_id =
+    (* 型変数のリスト *)
+    let tyvar_list = ref [ ] in
+      let gen_tyvar_id tyvar =
+        let rec index l counter = match l with
+          [ ] -> tyvar_list := List.append !tyvar_list [tyvar]; counter
+        | x :: rest -> 
+          if x = tyvar then counter else index rest (counter+1)
+        in index !tyvar_list 0
+      in gen_tyvar_id
+    in
+  (* int、bool、'a -> 'a のように表示する *)
+  let rec get_string t = match t with
+    TyInt -> "int"
+  | TyBool -> "bool"
+  | TyVar tyvar -> 
+    let tyvar_id = var_id tyvar in
+      (* tyvar_id が 25以下なら 'a, 'b, ... 'z *)
+      if tyvar_id <= 25 then "'" ^ Char.escaped (char_of_int ((int_of_char 'a') + tyvar_id))
+      (* 26個超過した時は 'a1, 'b1, ... 'z1, 'a2, ... と型変数を生成する *)
+      else "'" ^ Char.escaped (char_of_int ((int_of_char 'a') + (tyvar_id mod 26))) ^ (string_of_int (tyvar_id / 26))
   | TyFun (ty1, ty2) ->
-    print_string "(";
-    pp_ty ty1;
-    print_string ") -> ";
-    pp_ty ty2
+    let left = match ty1 with
+    (* かっこを付けるべきか否かの分岐 *)
+        TyFun (_, _) -> "(" ^ (get_string ty1) ^ ")"
+      | _ -> get_string ty1
+    in
+  let right = get_string ty2 in left ^ " -> " ^ right 
+    in print_string (get_string ty)
   ;;
+
 
 let rec freevar_ty ty = 
   match ty with
--- typing.ml
+++ typing.ml
@@ -7,6 +7,7 @@ let err s = raise (Error s)
 (* Type Environment *)
 type tyenv = ty Environment.t
 
+(*
 let ty_prim op ty1 ty2 = match op with
     Plus -> (match ty1, ty2 with
               TyInt, TyInt -> TyInt
@@ -18,41 +19,109 @@ let ty_prim op ty1 ty2 = match op with
               TyInt, TyInt -> TyBool
             | _ -> err ("Argument must be of integer: <"))
   | _ -> err "Not Implemented(´･ω･`)"
+  *)
+
+type subst = (tyvar * ty) list
+
+let rec subst_type subst t =
+  match t with
+    TyInt -> TyInt
+  | TyBool -> TyBool
+  | TyFun (arg1, arg2) -> TyFun ((subst_type subst arg1), (subst_type subst arg2))
+  | TyVar v -> (try subst_type subst (List.assoc v subst) with Not_found -> TyVar v)
+  ;;
+
+  
+(* eqs_of_subst : subst -> (ty * ty) list
+    型代入を型の等式集合に変換  *)
+let rec eqs_of_subst = function
+    [] -> []
+  | (tyvar, ty) :: rest -> (TyVar tyvar, ty) :: (eqs_of_subst rest)
+  ;;
+  
+  
+(* subst_eqs: subst -> (ty * ty) list -> (ty * ty) list
+  型の等式集合に型代入を適用 *)
+let rec subst_eqs s = function
+    [] -> []
+  | (t1, t2) :: rest -> (subst_type s t1, subst_type s t2) :: (subst_eqs s rest)
+
+
+(* 単一化 *)
+let rec unify l =
+  match l with
+    (* u(phi) = phi *)
+    [ ] -> [ ]
+  | (x :: rest) ->
+    match x with
+      (* u( {(tau, tau)} union+ X') = u(X') *)
+      (TyInt, TyInt) -> unify rest
+    | (TyBool, TyBool) -> unify rest
+    | (TyVar alpha, TyVar beta) when alpha = beta -> unify rest
+      (* u({(tau11->tau12, tau21->tau22)} union+ X') = u({(tau11, tau21),(tau12, tau22)} union+ X') *)
+    | (TyFun (a1, a2), TyFun (b1, b2)) -> unify ((a1, b1) :: ((a2, b2) :: rest))
+      (* u({(alpha, tau)} union+ X') or
+         u({(tau, alpha)} union+ X') *)
+    | (TyVar var1, TyVar var2) -> [(var1, TyVar var2)] @ unify (subst_eqs [(var1, TyVar var2)] rest)
+    | (TyVar var, ty) | (ty, TyVar var) -> 
+      (* if tau include alpha *)
+      if MySet.member var (Syntax.freevar_ty ty) then err ("Can't unify(´･ω･`)")
+      (* u({(alpha, tau)} union+ X') or
+         u({(tau, alpha)} union+ X') *)
+      else [(var, ty)] @ unify (subst_eqs [(var, ty)] rest)
+    | (_, _) -> err ("Can't unify(´･_･ `)")
+    ;;
+
+
+let ty_prim op ty1 ty2 = match op with
+    Plus | Mult -> ([(ty1, TyInt); (ty2, TyInt)], TyInt)
+  | Lt -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
+  | _ -> err "Not Implemented(´･ω･`)"
+
 
 let rec ty_exp tyenv = function
   Var x ->
-    (try Environment.lookup x tyenv with
+    (try ([], Environment.lookup x tyenv) with
       Environment.Not_bound -> err ("variable not bound: " ^ x))
-  | ILit _ -> TyInt
-  | BLit _ -> TyBool
+  | ILit _ -> ([], TyInt)
+  | BLit _ -> ([], TyBool)
   | BinOp (op, exp1, exp2) ->
-      let tyarg1 = ty_exp tyenv exp1 in
-      let tyarg2 = ty_exp tyenv exp2 in
-        ty_prim op tyarg1 tyarg2
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp tyenv exp2 in
+      let (eqs3, ty) = ty_prim op ty1 ty2 in
+      let eqs = (eqs_of_subst s1) @ (eqs_of_subst s2) @ eqs3 in
+      let s3 = unify eqs in (s3, subst_type s3 ty)
   | IfExp (exp1, exp2, exp3) ->
-      let tyarg1 = ty_exp tyenv exp1 in
-      let tyarg2 = ty_exp tyenv exp2 in
-      let tyarg3 = ty_exp tyenv exp3 in
-        (match tyarg1 with
-          TyBool -> if tyarg2 = tyarg3 then tyarg2 else err ("unmatched type")
-        | _ -> err ("condition in if expression must have the tyoe: bool")
-        )
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp tyenv exp2 in
+      let (s3, ty3) = ty_exp tyenv exp3 in
+      let eqs = (eqs_of_subst s1) @ (eqs_of_subst s2) @ (eqs_of_subst s3) @ 
+        [(ty1, TyBool)] @ [(ty2, ty3)] in
+      let s3 = unify eqs in (s3, subst_type s3 ty2)
   | LetExp (id, exp1, exp2) ->
-      let tyarg1 = ty_exp tyenv exp1 in
-        ty_exp (Environment.extend id tyarg1 tyenv) exp2
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp (Environment.extend id ty1 tyenv) exp2 in
+      let domty = TyVar (fresh_tyvar ()) in
+      let eqs3 = [(domty, ty1)] in
+      let eqs = (eqs_of_subst s1) @ (eqs_of_subst s2) @ eqs3 in
+      let s3 = unify eqs in
+      (s3, subst_type s3 ty2)
+  | FunExp (id, exp) ->
+    let domty = TyVar (fresh_tyvar ()) in
+    let s, ranty =
+      ty_exp (Environment.extend id domty tyenv) exp in
+      (s, TyFun (subst_type s domty, ranty))
+  | AppExp (exp1, exp2) -> 
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp tyenv exp2 in
+      let domty = TyVar (fresh_tyvar ()) in
+      let eqs = (ty1, TyFun(ty2, domty)) :: (eqs_of_subst s1) @ (eqs_of_subst s2) in
+      let s3 = unify eqs in
+      (s3, subst_type s3 domty)
   | _ -> err ("Not Implemented(´･ω･`)(´･ω･`)")
+
 
 let ty_decl tyenv = function
     Exp e -> ty_exp tyenv e
   | Decl (id, e) -> ty_exp tyenv e
   | _ -> err ("Not Implemented(´･ω･`)(´･ω･`)(´･ω･`)")
-
-type subst = (tyvar * ty) list
-
-let rec subst_type l t =
-  match t with
-    TyInt -> TyInt
-  | TyBool -> TyBool
-  | TyFun (arg1, arg2) -> TyFun ((subst_type l arg1), (subst_type l arg2))
-  | TyVar v -> (try subst_type l (List.assoc v l) with Not_found -> TyVar v)
-  ;;
