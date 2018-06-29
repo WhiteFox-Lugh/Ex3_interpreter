e82afe5691b603a81c92a4d27876e1e3131fc040
WhiteFox <lugh@h118.187.229.10.1017421.vlan.kuins.net>
2018-06-29 13:25:58 +0900

    Exercise 3.3.3 implemented

--- main.ml
+++ main.ml
@@ -20,6 +20,28 @@ let rec read_eval_print env =
     | Parsing.Parse_error -> err_process "Parsing error"
     | _ -> err_process "(´･ω･`)")
 
+
+(* batch interpreter *)
+let read_file_eval_print env =
+  let file = Sys.argv.(1) in
+  let open_file = open_in file in
+  (try 
+    let rec decl = Parser.toplevel Lexer.main (Lexing.from_channel open_file) in
+    let (id, newenv, v) = eval_decl env decl in
+    flush stdout;
+    Printf.printf "val %s = " id;
+    pp_val v;
+    print_newline();
+    exit 0
+  with End_of_file ->
+        close_in_noerr open_file;
+        raise End_of_file
+      | e ->
+        close_in_noerr open_file;
+        raise e)
+
+
+
 let initial_env = 
   Environment.extend "i" (IntV 1)
     (Environment.extend "v" (IntV 5) 
@@ -29,4 +51,7 @@ let initial_env =
           (Environment.extend "iii" (IntV 3) 
             (Environment.extend "iv" (IntV 4) Environment.empty)))))
 
-let _ = read_eval_print initial_env
+let _ = 
+  if (Array.length Sys.argv) = 1 then read_eval_print initial_env
+  else if (Array.length Sys.argv) = 2 then read_file_eval_print initial_env
+  else print_string "Error: the number of arguments must be less than two.\nman"
